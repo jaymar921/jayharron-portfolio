84f98eded3948f1c2d49634b5b3b2795a595e268
JayMar921 <jayharronabejar@gmail.com>
2026-01-27 15:48:21 +0800

    Minor UI changes

--- src/components/draggables/containers/DragContainer.jsx
+++ src/components/draggables/containers/DragContainer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function DragContainer({ children }) {
   return (
-    <div className="absolute z-[99] bg-[rgba(22,78,99,0.1)] text-white w-[200vw] h-screen select-none">
+    <div className="absolute z-[99] bg-[rgba(0,0,0,0.7)] text-white w-[200vw] h-screen select-none">
       {children}
     </div>
   );
