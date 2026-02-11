9c2de24eb02e5d1d68c028401803b9ce5099790f
JayMar921 <jayharronabejar@gmail.com>
2026-02-11 12:26:56 +0800

    Updated Drag Container bg-alpha

--- src/components/draggables/containers/DragContainer.jsx
+++ src/components/draggables/containers/DragContainer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function DragContainer({ children }) {
   return (
-    <div className="absolute z-[99] bg-[rgba(0,0,0,0.7)] text-white w-[200vw] h-screen select-none">
+    <div className="absolute z-[99] bg-[rgba(0,0,0,0.5)] text-white w-[200vw] h-screen select-none">
       {children}
     </div>
   );
